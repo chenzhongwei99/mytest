77fcd15bb582213e4cfb130d1f188f50b515de2e
chenzhongwei99 <czw880909@126.com>
2016-08-28 00:11:50 +0800

    test finall

---
+++ com-finall/com-finall-b/src/main/java/com/finall/b/App.java
@@ -0,0 +1,13 @@
+package com.finall.b;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
